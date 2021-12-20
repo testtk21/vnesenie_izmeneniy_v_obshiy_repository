5efe7bfdf4d520e906300a300ba0e68be6a29a91
Art20047483i7636 <92920990+Art20047483i7636@users.noreply.github.com>
2021-12-20 09:55:40 +0300

    Update test.py

--- test.py
+++ test.py
@@ -4,5 +4,18 @@ print('A * x^2 + B * x + C = 0\nВведите коэффициенты квад
 a = float(input('A = '))
 b = float(input('B = '))
 c = float(input('C = '))
+D = B**2 - 4 * a * c
+print ('Дискриминант D = ', D)
+if D > 0:
+    X1 = (-B + math.sqrt(D)) / 2 * a
+    X2 = (-B - math.sqrt(D)) / 2 * a
+    print ('X1=', X1)
+    print ('X2=', X2)
 
-#Вставить свой код здесь
+elif D == 0:
+    X = -B / 2 * a
+    print ('X=', X)
+    
+else:
+    print ('Вещественных корней нет')
+
