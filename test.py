5a476cf2c6562b472fb2110a3c85f58a446d95f3
VladaMakaruk <92307801+VladaMakaruk@users.noreply.github.com>
2021-12-20 09:38:18 +0300

    Update test.py

--- test.py
+++ test.py
@@ -6,3 +6,16 @@ b = float(input('B = '))
 c = float(input('C = '))
 
 #Вставить свой код здесь
+D=b**2-4*a*c
+print('D= '+str(D))
+
+if D>0:
+    x1=(-b+math.sqrt(D))/2*a
+    x2=(-b-math.sqrt(D))/2*a
+    print('x1= '+str(x1))
+    print('x2= '+str(x2))
+elif D==0:
+    x=-b/2*a
+    print('x= '+str(x))
+else:
+    print("Вещественных корней нет")
