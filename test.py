f9fe65c2e7d59e91e43e56e269fb4dfbb425a52c
AlekseyBirbon <92308290+AlekseyBirbon@users.noreply.github.com>
2021-12-20 09:18:32 +0300

    Update test.py

--- test.py
+++ test.py
@@ -6,3 +6,20 @@ b = float(input('B = '))
 c = float(input('C = '))
 
 #Вставить свой код здесь
+import math
+
+print('A * x**2 + B * x + C = 0\nВведите коэффиценты квадратного уравнения: ')
+a = float(input('A = '))
+B = float(input('B = '))
+c = float(input('C = '))
+D = B**2 - 4 * a * c
+if D > 0:
+    X1 = (-B + math.sqrt(D))/2*a
+    X2 = (-B - math.sqrt(D))/2*a
+    print(X1)
+    print(X2)
+elif D == 0:
+    X = - B/2*a
+    print(X)
+else:
+    print("вещественных корней нет")
